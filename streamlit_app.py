c9cca0d8066f36ddd6a68eda6571ace06a3ce219
Treyten <chiggy215@gmail.com>
2026-03-07 09:56:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Gym Performance Dashboard")
+
+@st.cache_data
+def load_data():
+    url = "https://raw.githubusercontent.com/chiggy215-spec/gym_performance_dashboard/main/data/Dashboard%20Exercise%20Data.xlsx"
+    df = pd.read_excel(url)
+    return df
+
+df = load_data()
+
+st.subheader("Data Preview")
+st.dataframe(df)
+
+st.subheader("New Members by Gym")
+
+fig = px.bar(
+    df,
+    x="Gym",
+    y="New Members",
+    color="Region"
 )
+
+st.plotly_chart(fig)
